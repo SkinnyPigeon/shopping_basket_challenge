cd4de26976982c9d38b947d02ef8cd0f2dbcc89a
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-08-30 13:22:05 +0100

    started to define the basket

--- models/Basket.java
+++ models/Basket.java
@@ -1,1 +1,25 @@
 package models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Basket {
+
+  private ArrayList< Consumable > mItems;
+  private double mTotal;
+
+  public Basket() {
+    mItems = new ArrayList< Consumable >();
+    mTotal = 0;
+  }
+
+  public double total() {
+    return mTotal;
+  }
+
+  public int size() {
+    return mItems.size();
+  }
+
+
+}
--- specs/BasketTest.java
+++ specs/BasketTest.java
@@ -1,14 +1,58 @@
-// import static org.junit.Assert.*;
-// import org.junit.*;
-// import models.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+import models.*;
 
-// public class BasketTest {
+public class BasketTest {
 
-//   // Basket basket;
+  Basket basket;
+  FoodFactory factory;
+  Consumable irnBru;
+  Consumable sprite;
+  Consumable walkers;
+  Consumable lays;
 
-//   // @Before
-//   // public void before(){
-//   //   basket = new Basket();
-//   // }
 
-// }
+  @Before
+  public void before() {
+    basket = new Basket();
+    factory = new FoodFactory();
+    irnBru = factory.getFood( "Fizzy Pop" );
+    sprite = factory.getFood( "Fizzy Pop" );
+    lays = factory.getFood( "Crisps" );
+    walkers = factory.getFood( "Crisps" );
+  }
+
+  @Test
+  public void basketStartsWithZeroTotal() {
+    assertEquals( 0, basket.total(), 0.01 );
+  }
+
+  @Test
+  public void basketStartsWithNoItems() {
+    assertEquals( 0, basket.size() );
+  }
+
+  // @Test
+  // public void basketCanAddConsumables() {
+  //   basket.add( irnBru );
+  //   assertEquals( 1, basket.size() );
+  // }
+
+  // @Test
+  // public void basketCanTotalUpGoods() {
+  //   basket.add( sprite );
+  //   basket.add( lays );
+  //   assertEquals( 1.5, basket.total(), 0.01 );
+  // }
+
+
+}
+
+
+
+
+
+
+
+
+
